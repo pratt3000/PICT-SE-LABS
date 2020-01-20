bbac92205011cd0dbbbd4ce72feddb95b80b5f3e
pratt <50728314+pratt3000@users.noreply.github.com>
2020-01-20 12:57:11 +0530

    20 January 2020 12:55pm
    -- lawl lagging behind a lot
    -- 3 new functions completed,
     hopefully next lab will be the last of this assignment lol

--- ADSL - 21163/a_4/src/a_4.cpp
+++ ADSL - 21163/a_4/src/a_4.cpp
@@ -13,11 +13,11 @@ class Node{
     string name;
     int fuel;
 public:
-    Node(string name){
+    Node(string name, int fuel){
         down = NULL;
         right = NULL;
         this->name = name;
-        fuel = 0;
+        this->fuel = fuel;
     }
     friend class AdjacencyList;
 };
@@ -33,54 +33,148 @@ public:
 	void initialise_vertices(){
 		Node *temp;
 		string name1;
-		cout<<"Enter number of vertices : ";
+		cout<<endl<<"Enter number of vertices : ";
 		cin>>vert;
+		cout<<"--------------------------------------------------"<<endl;
 		for(int i=0;i<vert;i++){
 			cout<<"\nEnter city name : ";
 			cin>>name1;
+			Node *t = new Node(name1, 0);
 			if(start == NULL){
-				start = new Node(name1);
+				start = t;
 				temp = start;
 			}
 			else{
-				temp->down = new Node(name1);
+				temp->down = t;
 				temp = temp->down;
 			}
 		}
-		cout<<"\nVertices initialised";
+		/*
+		cout<<start->name<<start->down->name;
+		if(start->down->down == NULL)
+			cout<<"fks";
+		*/
+		cout<<endl<<"--------------------------------------------------"<<endl;
 	}
-
 	void initialise_edges(){
 		Node *temp, *temp1;
 		string connec;
+		int fuel1;
 		temp = start;
-		for(int i=0;i<vert;i++){
-			cout<<"\nNo. of connections of "<<temp->name<<" : ";
-			cin>>edges;
-			for(int j=0; j<edges; j++){
-				cout<<"\n"<<j+1<<"th connection of "<<temp->name<<"  : ";
+		while(temp != NULL){
+			edges = vert+1;
+			while(edges >= vert){
+				cout<<"\nNo. of connections of "<<temp->name<<" : ";
+				cin>>edges;
+			}
+			while(edges--){
+				e:cout<<"Connection of "<<temp->name<<" : ";
 				cin>>connec;
-				cout<<"Fuel needed (litres) : ";
-				cin>>temp->fuel;
-				if(!alreadyPresent(temp, connec)){
+				if(!amongEdges(temp, connec) && amongVertices(connec)){
+					cout<<"Fuel needed (litres) : ";
+					cin>>fuel1;
+					cout<<endl;
 					temp1 = temp;
 					while(temp1->right != NULL)
 						temp1 = temp1->right;
-					temp1->right = new Node(connec);
+					temp1->right = new Node(connec, fuel1);
+				}
+				else{
+					cout<<"******re-enter******"<<endl;
+					goto e;
 				}
 			}
 			temp = temp->down;
 		}
 	}
-	bool alreadyPresent(Node *temp, string connec){
+	bool amongEdges(Node *temp, string connec){
 		bool present = 0;
-		while(temp->right != NULL){
-			if(!temp->name.compare(connec) == 0)
+		while(temp != NULL){
+			if(temp->name.compare(connec) == 0){
 				present = 1;
+				break;
+			}
 			temp = temp->right;
 		}
 		return present;
 	}
+	bool amongVertices(string connec){
+		Node*temp = start;
+		bool present = 0;
+		while(temp != NULL){
+			if(temp->name.compare(connec) == 0){
+				present = 1;
+				break;
+			}
+			temp = temp->down;
+		}
+		return present;
+	}
+	void addEdge(){
+		string from, to;
+		int fuel1;
+		cout<<"\nAdding and edge";
+		cout<<"\nFROM : ";
+		cin>>from;
+		cout<<"\nTO : ";
+		cin>>to;
+		Node *tempdn = start;
+		if(amongVertices(from)){
+			while(tempdn != NULL){
+				if(tempdn->name.compare(from) == 0)
+					break;
+				tempdn = tempdn->down;
+			}
+			Node *temprt = tempdn;
+			if(!amongEdges(temprt, to)){
+				while(temprt->right != NULL){
+					temprt = temprt->right;
+				}
+				cout<<"\nFuel needed (litres) : ";
+				cin>>fuel1;
+				temprt->right = new Node(to, fuel1 );
+			}
+			else{
+				cout<<"\n******Edge already present******";
+			}
+		}
+		else{
+			cout<<"\n******Start vertice not present******";
+		}
+	}
+	void deleteEdge(){
+		string from, to;
+		int fuel1;
+		cout<<"\nAdding and edge";
+		cout<<"\nFROM : ";
+		cin>>from;
+		cout<<"\nTO : ";
+		cin>>to;
+		Node *tempdn = start;
+		if(amongVertices(from)){
+			while(tempdn != NULL){
+				if(tempdn->name.compare(from) == 0)
+					break;
+				tempdn = tempdn->down;
+			}
+			Node *temprt = tempdn, *rebound;
+			if(amongEdges(temprt, to)){
+				while(temprt != NULL){
+					if(temprt->name.compare(to) == 0)
+						break;
+					rebound = temprt;
+					temprt = temprt->right;
+				}
+				rebound->right = temprt->right;
+				delete temprt;
+			}
+			else
+				cout<<"\n******Edge not present******";
+		}
+		else
+			cout<<"\n******Edge not present******";
+	}
+
 };
 
 
@@ -89,26 +183,27 @@ int main() {
 	int cont = 1;
 	int choice;
 	cout<<"\n1 : Initialise directed Graph";
-	cout<<"\n2 : ";
-	cout<<"\n3 : ";
-	cout<<"\n4 : ";
-	cout<<"\n5 : ";
-	cout<<"\n6 : ";
-	cout<<"\n7 : ";
+	cout<<"\n2 : Add Edge";
+	cout<<"\n3 : Delete Edge";
+	cout<<"\n4 : Add Vertice";
+	cout<<"\n5 : Delete Vertice";
+	cout<<"\n6 : In-degree & Out-degree";
+	cout<<"\n7 : Traversal";
 	cout<<"\n99 : END"<<endl;
 	while(cont){
 		cout<<"--- Enter choice :  ";
 		cin>>choice;
+		cout<<"========================================================="<<endl;
 		switch(choice){
 			case 1:
 				ob.initialise_vertices();
 				ob.initialise_edges();
 				break;
 			case 2:
-
+				ob.addEdge();
 				break;
 			case 3:
-
+				ob.deleteEdge();
 				break;
 			case 4:
 
